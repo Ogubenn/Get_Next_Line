93d44fca0bccb211935e84df11a32517c3da222d
Ogulcan Durkan <ogdurkan@k2m16s01.42kocaeli.com.tr>
2022-10-27 14:32:36 +0300

    27/10/2022-14:32

--- get_next_line.c
+++ get_next_line.c
@@ -70,9 +70,39 @@ char	*move_line(char *s)
 	free(s);
 	return (dest);
 }
+//Normal olarak, içinde lokal değişken tanımlanan
+ //bir fonksiyonu her çağırdığımızda, lokal değişken değeri yenilenir. 
+ //Ancak, bu lokal değişkeni static olarak tanımlarsak, fonksiyonu her çağırmamızda, 
+ //lokal değişken bir önceki fonksiyon çağrısındaki en son değerini korur.
+char	*get_next_line(int fd)
+{
+	static char	*s;
+	char		*dest;
+	char		*buffer;
+	int			size;
+
+	buffer = malloc(BUFFER_SIZE + 1);
+	size = read(fd, buffer, BUFFER_SIZE);
+	buffer[size] = 0;
+	while (size > 0)
+	{
+		if (!s)
+			s = ft_strdup(buffer);
+		else
+			s = ft_strjoin(s, buffer);
+		if (ft_strchr(buffer, '\n'))
+			break ;
+		size = read(fd, buffer, BUFFER_SIZE);
+		buffer[size] = 0;
+	}
+	dest = ft_substr(s, 0, ft_strchr(s, '\n') - s + 1);
+	s = move_line(s);
+	free(buffer);
+	return (dest);
+}
 #include <stdio.h>
 int main()
 {
-    char *a = "\nOGubenn\n";
+    char a[] = "aasd\nssssss\nasdasd";
     printf("%s\n",move_line(a));
 }
